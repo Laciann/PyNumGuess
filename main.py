1a8205a39dd82c939bff9a20da5a8c283bc001ab
Abhishek Acharya <abhishek.acharya@apexcollege.edu.np>
2022-05-29 22:15:56 +0545

    logic

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from __future__ import print_function
+from ast import While
 import random
 
 number_of_guesses = 0 
@@ -5,4 +7,30 @@ number = random.randint(1,50)
 
 name = input("Your name? ")
 
-print("Hello ", name, "! I have selected an integer number between 1 to 50. Take a guess! What must be it?")
+print("Hello", name + "! I have selected an integer number between 1 to 50. Take a guess! What must be it?")
+
+while number_of_guesses < 8:
+    guess = input("Enter your guess ")
+    guess = int(guess)
+
+    number_of_guesses = number_of_guesses + 1 
+    guesses_left = 8 - number_of_guesses
+
+    if guess < number:
+        guesses_left = str(guesses_left)
+        print("Guess is lower! You have " + guesses_left + " guesses left.")
+
+    elif guess > number:
+        guessesLeft=str(guesses_left)
+        print("Guess is higher! You have " + guesses_left + " guesses left.")
+
+    elif guess == number:
+        break
+
+if guess==number:
+  number_of_guesses=str(number_of_guesses)
+  print("Good job! It took " + number_of_guesses + " attempts for you to guess correctly.")
+
+elif guess!=number:
+  number=str(number)
+  print("Sorry. The number actually is " + number + ".")
