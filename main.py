ce79828584c75293c14afdedecbec65843b7dd86
Abhishek Acharya <abhishek.acharya@apexcollege.edu.np>
2022-05-29 21:49:08 +0545

    intial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+import random
+
+number_of_guesses = 0 
+number = random.randint(1,50)
+
+name = input("Your name? ")
+
+print("Hello ", name, "! I have selected an integer number between 1 to 50. Take a guess! What must be it?")
